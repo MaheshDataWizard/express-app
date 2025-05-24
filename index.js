09cc42724b2945a5fb48665699a71899bb2eac19
Mahesh Khatik <maheshkhatikcareers@gmail.com>
2025-05-24 11:21:01 +0530

    dynamic route handling and query string

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
-// Contact route
-app.get("/contact", (req, res) => {
-  res.send("This is the contact route");
+// Dynamic routing in express
+app.get("/user/:username", (req, res) => {
+  const username = req.params.username;
+  res.send(`Welcome ${username}`);
 });
 
-// About route
-app.get("/about", (req, res) => {
-  res.send("This is the about route");
+// /search?keyword=express
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  res.send(`Searching for ${keyword}`);
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
